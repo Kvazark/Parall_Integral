6871580517bd1c80687229d4db35038880bba8f9
first <Katya_Klebleeva@mail.ru>
2022-01-13 11:41:27 +0300

    Add project.

---
+++ Integral/Integral.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Integral
+{
+    public class Integral
+    {
+        private readonly Func<double, double> _func;
+        private readonly double _left;
+        private readonly double _right;
+        private readonly double _e; //погрешность
+        private readonly double _n; 
+
+        private double _result;
+
+        public Integral(Func<double, double> function, double left, double right, double e, int n)
+        {
+            _func = function;
+            _left = left;
+            _right = right;
+            _e = e;
+            _n = n;
+        }
+
+        //Метод средних прямоугольников с точностью е
+        public double CalculationNoParall()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            double result = 0;
+
+            for (double i = _left; i < _right; i += _e)
+            {
+                double h = ((i + _e) - i) / _n;
+                double x = i + h;
+                result += _func(x) * _e;
+            }
+
+            watch.Stop();
+
+            Console.WriteLine("Время вычисления в однопоточном режиме: " + watch.ElapsedMilliseconds);
+            Console.WriteLine($"Результат: {Math.Round(result, 7)}.");
+            return result;
+        }
+
+
+        public void CalculateIntegral(double left, double right, out double result)
+        {
+            var h = right - left / _n; //шаг разбиения отрезка
+            var center = left + h;
+            
+            CalculateIntegral(left, center, out var s1);
+            CalculateIntegral(center, right, out var s2);
+
+            result = s1 + s2;
+        }
+
+        public void Parall()
+        {
+        }
+    }
+}
---
+++ Integral/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Integral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Интеграл: 18*x/sqrt(x^2+9)");
+            Console.Write("Введите левую границу интегрирования: ");
+            double _left = double.Parse(Console.ReadLine());
+            Console.Write("Введите правую границу интегрирования: ");
+            double _right = double.Parse(Console.ReadLine());;
+
+            var integral = new Integral(
+                function: x => 18 * x / Math.Sqrt(Math.Pow(x, 2)+9),
+                left: _left,
+                right: _right,
+                e: 0.001,
+                n: 2
+            );
+            integral.CalculationNoParall();
+
+        }
+        
+    }
+}
